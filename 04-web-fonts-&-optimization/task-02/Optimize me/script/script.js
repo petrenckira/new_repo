8502316c8aacb07494ebdc64925619866cf0df81
Iryna Petrenko1 <iryna_petrenko1@epam.com>
2017-07-25 18:07:19 +0300

    adding tasks Petrenko1

---
+++ 04-web-fonts-&-optimization/task-02/Optimize me/script/script.js
@@ -0,0 +1,45 @@
+var Mortgage = function(name) {
+    this.name = name;
+}
+
+Mortgage.prototype = {
+
+    applyFor: function(amount) {
+        // access multiple subsystems...
+        var result = "approved";
+        if (!new Bank().verify(this.name, amount)) {
+            result = "denied";
+        } else if (!new Credit().get(this.name)) {
+            result = "denied";
+        } else if (!new Background().check(this.name)) {
+            result = "denied";
+        }
+        return this.name + " has been " + result +
+            " for a " + amount + " mortgage";
+    }
+}
+
+var Bank = function() {
+    this.verify = function(name, amount) {
+        return true;
+    }
+}
+
+var Credit = function() {
+    this.get = function(name) {
+        return true;
+    }
+}
+
+var Background = function() {
+    this.check = function(name) {
+        return true;
+    }
+}
+
+function run() {
+    var mortgage = new Mortgage("Joan Templeton");
+    var result = mortgage.applyFor("$100,000");
+
+    console.log(result);
+}
